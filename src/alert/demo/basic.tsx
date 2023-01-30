7f0223cbefe6098f1df4439598bfa691561d619a
faziji <Zhu799353764@qq.com>
2023-01-30 14:35:57 +0800

    chore:add

--- src/alert/demo/basic.tsx
+++ src/alert/demo/basic.tsx
@@ -1,11 +1,16 @@
 // import React from 'react';
 
-// import { Alert } from '@zzzzw/happy-ui';
 
 // export default () => <Alert kind="warning">这是一条警告提示</Alert>;
 
 import React from 'react';
 import Alert from '../index';
+// import Alert from 'happy-ui';
+// import Alert from '../../../esm/alert';
+// import { Alert } from '@faziji/happy-ui';
+// import { Alert } from 'happy-ui';
 import '../style';
+// import 'happy-ui/esm/alert/style';
+
 
 export default () => <Alert kind="warning">这是一条警告提示</Alert>;
